9c3e76c326631ccdc2d8ec135c9562149cc91a61
Desterothx <60052867+Desterothx@users.noreply.github.com>
2023-11-29 03:29:19 +0100

    Update upgrades.js

--- modules/upgrades.js
+++ modules/upgrades.js
@@ -190,7 +190,7 @@ function buyUpgrades() {
 		var fuckbuildinggiga = (bwRewardUnlocked("AutoStructure") && bwRewardUnlocked("DecaBuild") && getPageSetting('buildingsType') === 0);
 		if (upgrade === 'Coordination') {
 			//Coord & Amals
-			if (upgradeSetting === 2 || !canAffordCoordinationTrimps() || mojHDcalctest<.01) continue;
+			if (upgradeSetting === 2 || !canAffordCoordinationTrimps() || mojHDcalctest<.0005) continue;
 			//Skip coords if we have more than our designated cap otherwise buy jobs to ensure we fire enough workers for the coords we want to get.
 			if (challengeActive('Trappapalooza') || challengeActive('Trapper') && getPageSetting(trimpStats.currChallenge.toLowerCase())) {
 				if (getPageSetting(trimpStats.currChallenge.toLowerCase() + 'Coords') > 0 && game.upgrades.Coordination.done >= getPageSetting(trimpStats.currChallenge.toLowerCase() + 'Coords')) continue;
